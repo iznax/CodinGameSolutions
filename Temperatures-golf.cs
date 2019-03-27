e4b20145ebcec8b2e58b8d6db4266aae2ac804f7
iznax <iznax@users.noreply.github.com>
2019-03-26 22:50:01 -0500

    Update Temperatures-golf.cs

--- Temperatures-golf.cs
+++ Temperatures-golf.cs
@@ -1,1 +1,1 @@
-namespace System.Linq{class P{static void Main(){Console.Write((Console.ReadLine()!="0"?Console.ReadLine():"0").Split(' ').Select(int.Parse).OrderBy(x=>-x*x+x).Last());}}}
+namespace System.Linq{class P{static void Main(){Console.Write(Console.ReadLine()=="0"?0:Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x=>-x*x+x).Last());}}}
